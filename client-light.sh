e09d5b72a6a8435bcd1ffde053738c6bd9f1b560
msdx321 <msdx321@gmail.com>
2021-10-13 00:11:49 +0000

    Add some scripts

---
+++ client-light.sh
@@ -0,0 +1,1 @@
+sudo ./build/pingpong --file-prefix=client-light -w 0b:00.0 -l 2,3 -- -p 0 -n 200 --client_mac=00:0c:29:97:1a:2a --server_mac=00:0c:29:97:1a:34
---
+++ server.sh
@@ -0,0 +1,1 @@
+sudo ./build/pingpong --file-prefix=server -w 13:00.0 -l 0,1 -- -p 0 -s --server_mac=00:0c:29:97:1a:34
