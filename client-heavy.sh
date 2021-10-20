f9d00d26874cd71f281a7f15e708e1d6456f9d73
msdx321 <msdx321@gmail.com>
2021-10-20 01:54:05 -0400

    Remove scripts
    Signed-off-by: msdx321 <msdx321@gmail.com>

--- client-heavy.sh
+++
@@ -1,1 +0,0 @@
-sudo ./build/pingpong --file-prefix=client-heavy -w 1b:00.0 -l 4,5 -- -p 0 -n 200 -h --client_mac=00:0c:29:97:1a:3e --server_mac=00:0c:29:97:1a:34
--- client-light.sh
+++
@@ -1,7 +0,0 @@
-sudo ./build/pingpong --file-prefix=client \
-    --socket-mem=1024 \
-    --no-pci \
-    --lcores '0@(0-5)' \
-    --vdev 'net_virtio_user2,mac=00:00:00:00:00:02,path=/var/run/openvswitch/vhost-user2' \
-    -- \
-    -p 0 -n 20000 --client_mac=00:00:00:00:00:02 --server_mac=00:00:00:00:00:01
